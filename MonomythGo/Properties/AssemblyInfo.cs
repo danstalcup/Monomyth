f8fd4404ad9895fa0a3bc5c0f43a94adace2b472
danstalcup <billyjoelntrdame@gmail.com>
2017-08-16 20:40:58 -0400

    Initial commit for run, class, test projects

--- MonomythGo/Properties/AssemblyInfo.cs
+++ MonomythGo/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Monomyth")]
+[assembly: AssemblyTitle("MonomythGo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Monomyth")]
+[assembly: AssemblyProduct("MonomythGo")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("71f1d26e-3418-42b9-a130-d74f48868945")]
+[assembly: Guid("18195693-dafd-455e-8f5c-7ebf8c399757")]
 
 // Version information for an assembly consists of the following four values:
 //
