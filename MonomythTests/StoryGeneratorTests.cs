2b857db57fa52b548927e02d50531b6ddff61ede
danstalcup <billyjoelntrdame@gmail.com>
2017-08-16 20:48:32 -0400

    Add Data Project. Add StoryGenerator. Add first tests.

---
+++ MonomythTests/StoryGeneratorTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Monomyth.Impl;
+using NUnit;
+using NUnit.Framework;
+
+namespace MonomythTests
+{
+    [TestFixture]
+    public class StoryGeneratorTests
+    {
+        [Test]
+        public void StoryGenerator_GenerateStory_NotNull()
+        {
+            // act
+            var result = new StoryGenerator().GenerateStory();
+
+            // assert
+            
+        }
+
+    }
+}
